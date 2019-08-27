aa15315cadfa70496faa614b11deca92edfe88fa
BillyGun27 <billygun27@gmail.com>
2019-08-27 21:29:16 +0700

    Create TripleSum.java

---
+++ TripleSum.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the triplets function below.
+    static long triplets(int[] aa, int[] bb, int[] cc) {
+        //Arrays.sort(a);
+        //Arrays.sort(b);
+        //Arrays.sort(c);
+        int[] a = Arrays.stream(aa).distinct().sorted().toArray() ;
+        int[] b = Arrays.stream(bb).distinct().sorted().toArray() ;
+        int[] c = Arrays.stream(cc).distinct().sorted().toArray() ;
+
+        //int[][] triple= new int[b.length][2];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        long total=0;
+        while(j<b.length){
+            while(i<a.length && a[i] <= b[j]){
+                i++;
+            }
+
+            while(k<c.length && c[k] <= b[j]){
+                k++;
+            }
+
+            total += Long.valueOf(i)*Long.valueOf(k);
+            j++;
+
+        }
+
+      
+        return total;
+
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] lenaLenbLenc = scanner.nextLine().split(" ");
+
+        int lena = Integer.parseInt(lenaLenbLenc[0]);
+
+        int lenb = Integer.parseInt(lenaLenbLenc[1]);
+
+        int lenc = Integer.parseInt(lenaLenbLenc[2]);
+
+        int[] arra = new int[lena];
+
+        String[] arraItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lena; i++) {
+            int arraItem = Integer.parseInt(arraItems[i]);
+            arra[i] = arraItem;
+        }
+
+        int[] arrb = new int[lenb];
+
+        String[] arrbItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenb; i++) {
+            int arrbItem = Integer.parseInt(arrbItems[i]);
+            arrb[i] = arrbItem;
+        }
+
+        int[] arrc = new int[lenc];
+
+        String[] arrcItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenc; i++) {
+            int arrcItem = Integer.parseInt(arrcItems[i]);
+            arrc[i] = arrcItem;
+        }
+
+        long ans = triplets(arra, arrb, arrc);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
