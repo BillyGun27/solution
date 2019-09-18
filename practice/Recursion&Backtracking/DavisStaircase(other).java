8affdaffec2a394af043b81c18883d894b5cd422
BillyGun27 <billygun27@gmail.com>
2019-09-18 08:26:09 +0700

    Update and rename practice/tempo.java to practice/Recursion&Backtracking/DavisStaircase(other).java

--- practice/Recursion&Backtracking/DavisStaircase(other).java
+++ practice/Recursion&Backtracking/DavisStaircase(other).java
@@ -8,32 +8,16 @@ import java.util.regex.*;
 
 public class Solution {
     public static int total;
-
-    static void stepCom(int s,int prev,int curr, int[] arr,int[] arthree,int max){
+    //trying back tracking but fail
+    static void stepCom(int s,int max){
         if(s > max){
             return;
         }else if(s == max){
-            for(int i=0;i<curr;i++){
-                System.out.print(arr[i]+" ");
-                //arr[i] = 0;
-            }
-
-            System.out.println("|"+s+"|");
-            
-            for(int i=0;i<4;i++){
-                System.out.print(arthree[i]+"k");
-                arthree[i] = 0;
-            }
-
-            System.out.println("|x"+s+"x|");
-
             total++;
             return;
         }else{
-            for(int i=prev;i<4;i++){
-                arr[curr] = i;
-                arthree[i]++;
-                stepCom( s+i,i,curr+1, arr,arthree ,max);
+            for(int i=1;i<4;i++){
+                stepCom( s+i,max);
             }
         }
     }
@@ -41,9 +25,7 @@ public class Solution {
     // Complete the stepPerms function below.
     static int stepPerms(int n) {
         total = 0;
-        int[] arr = new int[n];
-        int[] arthree = new int[4];
-        stepCom(0,1,0,arr,arthree,n);
+        stepCom(0,n);
         return total;
 
     }
