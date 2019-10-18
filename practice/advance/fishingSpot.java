37d79be3bc063f0bfdf495822d895b484d1172a8
BillyGun27 <billygun27@gmail.com>
2019-10-18 17:48:04 +0700

    Update fishingSpot.java

--- practice/advance/fishingSpot.java
+++ practice/advance/fishingSpot.java
@@ -22,9 +22,36 @@ import java.util.*;
 
 public class Solution {
 
-    static void searchSpot(int[][] Gate,int TotalSpot,int[] visitedSpot,int[] visitedGate){
+    static void searchSpot(int[][] Gate,int TotalSpot,int[] visitedSpot,int[] visitedGate, String[] orderText , int dist){
+        if(dist >= 3){
+            for(int i=0;i<3;i++){
+                System.out.print(orderText[i]+" ");
+            }
+            System.out.println(" finish");
+            return;
+        }
         
+
         for(int i=0;i<3;i++){
+            if(visitedGate[i] == 0){
+
+                visitedGate[i] = 1;
+                /*
+                if( Gate[i][1] % 2 == 0 ){
+                    System.out.println(current+" "+i+" right left");
+                }else{
+                    System.out.println(current+" "+i);
+                }*/
+                orderText[dist] = i+"r";
+                searchSpot(Gate,TotalSpot,visitedSpot,visitedGate,orderText,dist+1);
+
+                orderText[dist] = i+"l";
+                searchSpot(Gate,TotalSpot,visitedSpot,visitedGate,orderText,dist+1);
+                visitedGate[i] = 0;
+
+            }
+
+            /*
             int fish = 0;
             int pos = 0;
 
@@ -47,11 +74,14 @@ public class Solution {
 
                 pos++;
             }
-        }
+            */
 
+        }
+        
+        /*
         for(int i=1;i<visitedSpot.length;i++){
             System.out.print(visitedSpot[i]+" ");
-        }
+        }*/
 
     }
 
@@ -69,9 +99,12 @@ public class Solution {
                 Gate[i][j] = scanner.nextInt();
             }
         }
+
         int[] visitedSpot = new int[TotalSpot+1];
         int[] visitedGate = new int[3];
-        searchSpot(Gate,TotalSpot,visitedSpot,visitedGate);
+        String[] orderText = new String[3];
+        int dist = 0 ;
+        searchSpot(Gate,TotalSpot,visitedSpot,visitedGate,orderText, dist);
         System.out.println(Gate[0][0]); 
     }
 }
