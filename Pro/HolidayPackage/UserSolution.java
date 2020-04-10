5c5a5d4a33e1ef32e262bcbc547024e9745951b7
BillyGun27 <billygun27@gmail.com>
2020-04-10 17:02:05 +0700

    Update UserSolution.java

--- Pro/HolidayPackage/UserSolution.java
+++ Pro/HolidayPackage/UserSolution.java
@@ -59,6 +59,7 @@ class UserSolution
 		UserList[uid1].friends[uid2] = 1;
 		UserList[uid2].friends[uid1] = 1;
 		
+		
 	}
 	
 	public void add(int pid, int area, int price)
@@ -72,13 +73,13 @@ class UserSolution
 		}else {
 			while(selectedAddr != null) {
 				if(selectedAddr.next == null) {
+					selectedAddr.next = new pack(pid,price,area);
 					break;
 				}
 				
 				selectedAddr = selectedAddr.next;
 			}
 			
-			selectedAddr.next = new pack(pid,price,area);
 		}
 		
 		
@@ -115,19 +116,7 @@ class UserSolution
 			
 			
 		}
-		/*
-		if(area == 8) {
-			System.out.println("add");
-			pack tempo = PackageList[area];
-			
-			while(tempo != null) {
-				
-				System.out.println("id:"+tempo.id+" price:"+tempo.price+" "+area);
-				
-				tempo = tempo.next;
-			}
-			System.out.println("add--");
-		}*/
+	
 		
 		
 	}
@@ -156,29 +145,14 @@ class UserSolution
 		}else {
 			prevAddr.next = selectedAddr.next;
 		}
-		/*
-		if(uid==723 && pid==604351784) {
-			
-			System.out.println("first"+PackageList[selectedArea].id+" "+selectedArea);
-			pack tempo = PackageList[selectedArea];
-					
-			while(tempo != null) {
-				
-				System.out.println("id:"+tempo.id+" price:"+tempo.price+" "+selectedArea);
-				
-				tempo = tempo.next;
-			}
-		}*/
+		
 		
 		pack selectedPack = PackageList[selectedArea];
 		pack prev = null;
 		while(selectedPack != null) {
 			
 			if(selectedPack.id == pid) {
-				/*
-				if(uid==723 && pid==604351784) {
-					System.out.println("other"+selectedPack.id +" "+selectedArea);
-				}*/
+				
 				break;
 			}
 			
@@ -187,73 +161,22 @@ class UserSolution
 		}
 		
 		if(prev == null) {
-			/*
-			if(uid==723 && pid==604351784) {
-				System.out.println("prev");
-			}*/
+			
 			PackageList[selectedArea] = selectedPack.next;
 		}else {
-			/*
-			if(uid==723 && pid==604351784) {
-				System.out.println("second prev ");
-				if(selectedPack.next==null) {
-					System.out.println("second prev is null"+prev.id);
-				}
-			}*/
+			
 			prev.next = selectedPack.next;
 		}
 		
-		/*
-		if(uid==723 && pid==604351784) {
-			System.out.println("second "+PackageList[selectedArea].id+" "+selectedArea);
-			pack tempo = PackageList[selectedArea];
-			
-			while(tempo != null) {
-				
-				System.out.println("id:"+tempo.id+" price:"+tempo.price+" "+selectedArea);
-				
-				tempo = tempo.next;
-			}
-		}*/
 		
 		if(UserList[uid] == null) {
 			//System.out.println("reserve uid "+uid);
 			UserList[uid] = new user(userLen,areaLen);
 		}
 		
-		boolean check = false;
-		if(uid == 535) {
-			check = true;
-		}
+	
 		UserList[uid].area[selectedArea]++;
-		int ft = UserList[uid].friends.length;
-		for(int i=1;i<ft;i++) {
-			if(UserList[uid].friends[i] == 1) {
-				int fuid = i;
-				if(UserList[fuid] == null) {
-					UserList[fuid] = new user(userLen,areaLen);
-				}
-				UserList[fuid].area[selectedArea]++;
-				if(fuid == 535) {
-					check = true;
-				}
-				//System.out.println("result : "+fuid+" "+ UserList[fuid].area[selectedArea] );
-			}
-			 
-		}
 		
-		
-		
-		if(selectedArea == 4 && check ) {
-			System.out.println("id "+uid);
-			//for(int i=1;i<areaLen;i++) {
-			System.out.println("area: "+4+" total:"+UserList[535].area[4]);
-				 
-			//}
-			System.out.println("id--");
-		}
-		
-		//System.out.println("result : "+uid+" "+ UserList[uid].area[selectedArea] );
 		
 	}
 	
@@ -265,138 +188,69 @@ class UserSolution
 			UserList[uid] = new user(userLen,areaLen);
 		}
 		
-		int[] Visited = new int[areaLen];
-		int[] AreaTotal = UserList[uid].area;
+		int[] AreaTotal = new int[areaLen];
+				
+		for(int j=1;j<areaLen;j++) {
+				AreaTotal[j] += UserList[uid].area[j];
+		}	
+		
+		
+		int ft = UserList[uid].friends.length;
+		for(int i=1;i<ft;i++) {
+			if(UserList[uid].friends[i] == 1) {
+				int fuid = i;
+				
+				for(int j=1;j<areaLen;j++) {
+					AreaTotal[j] += UserList[fuid].area[j];
+				}
+			}	 
+		}
 		
 		int selectedArea = 0;
-		int checked = 0;
 		
-		//if(first) {
-		/*for(int i=1;i<areaLen;i++) {
-			if(PackageList[i] != null) {
-				System.out.print("id:"+PackageList[i].id+" price:"+PackageList[i].price);
-			}else {
-				System.out.print("id: - ");
-			}
-			System.out.println(" check total_visit:"+AreaTotal[i]+" area:"+i);
-		}*/
-		//}
 		
-		//System.out.print("user:"+ uid+" |");
-		while(checked<areaLen) {
-			int max = -1;
-			int minPrice = Integer.MAX_VALUE;
-			for(int i=1;i<areaLen;i++) {
-				//System.out.println("check "+AreaTotal[i]+" "+i);
-				if( max<AreaTotal[i] && Visited[i] == 0 && PackageList[i] != null) {
-					//System.out.println("max "+AreaTotal[i]+" "+i);
+		int max = -1;
+		int minPrice = Integer.MAX_VALUE;
+		int curPId = 0;
+		for(int i=1;i<areaLen;i++) {
+			
+			if( max<AreaTotal[i]  && PackageList[i] != null) {//
+				
+				max = AreaTotal[i];
+				selectedArea = i;
+				minPrice =  PackageList[selectedArea].price;
+				curPId =  PackageList[selectedArea].id;
+			}else if(max == AreaTotal[i] && PackageList[i] != null) {//&& Visited[i] == 0 
+				
+				if(minPrice > PackageList[i].price ) {
 					max = AreaTotal[i];
 					selectedArea = i;
 					minPrice =  PackageList[selectedArea].price;
-				}else if(max == AreaTotal[i] && Visited[i] == 0 && PackageList[i] != null) {
-					//System.out.println("other max "+AreaTotal[i]+" "+i+"p "+minPrice+" np"+PackageList[i].price);
-					if(minPrice > PackageList[i].price ) {
-						max = AreaTotal[i];
-						selectedArea = i;
-						minPrice =  PackageList[selectedArea].price;
-					}
+					curPId =  PackageList[selectedArea].id;
+				}else if(minPrice == PackageList[i].price && curPId > PackageList[i].id) {
+					max = AreaTotal[i];
+					selectedArea = i;
+					minPrice =  PackageList[selectedArea].price;
+					curPId =  PackageList[selectedArea].id;
 				}
 				
 			}
 			
-			//System.out.println("user:"+uid+" selected area:"+selectedArea +" ");
-			Visited[selectedArea] = 1;
-			if(PackageList[selectedArea] != null) {
-				//System.out.print("id:"+PackageList[selectedArea].id  +" ");
-				//if(first) {
-				//	System.out.println("user:"+uid+" area: "+selectedArea+" recommend:"+PackageList[selectedArea].id);
-					//System.out.println("ll");
-				//	first = false;
-				//}
-				
-				
-				return PackageList[selectedArea].id ;
-			}
-			
-			//System.out.println();
-			checked++;
-			
 		}
-		//System.out.println("recommend fail");
+		
+		
+		
+		if(PackageList[selectedArea] != null) {
+			
+			return PackageList[selectedArea].id ;
+		}
+			
+	
 		return 0;
 		
 	}
 	
-	public int recommend_debug(int uid)
-	{	
-		if(UserList[uid] == null) {
-			//System.out.println("recommend uid "+uid);
-			UserList[uid] = new user(userLen,areaLen);
-		}
-		
-		int[] Visited = new int[areaLen];
-		int[] AreaTotal = UserList[uid].area;
-		
-		int selectedArea = 0;
-		int checked = 0;
-		
-	//	if(first) {
-		System.out.println("compare");
-			for(int i=1;i<areaLen;i++) {
-				if(PackageList[i] != null) {
-					System.out.print("id:"+PackageList[i].id+" price:"+PackageList[i].price);
-				}else {
-					System.out.print("id: - ");
-				}
-				System.out.println(" check total_visit:"+AreaTotal[i]+" area:"+i);
-			}
-		//}
-		
-		//System.out.print("user:"+ uid+" |");
-		while(checked<areaLen) {
-			int max = -1;
-			int minPrice = Integer.MAX_VALUE;
-			for(int i=1;i<areaLen;i++) {
-				//System.out.println("check "+AreaTotal[i]+" "+i);
-				if( max<AreaTotal[i] && Visited[i] == 0 && PackageList[i] != null) {
-					//System.out.println("max "+AreaTotal[i]+" "+i);
-					max = AreaTotal[i];
-					selectedArea = i;
-					minPrice =  PackageList[selectedArea].price;
-				}else if(max == AreaTotal[i] && Visited[i] == 0 && PackageList[i] != null) {
-					//System.out.println("other max "+AreaTotal[i]+" "+i+"p "+minPrice+" np"+PackageList[i].price);
-					if(minPrice > PackageList[i].price ) {
-						max = AreaTotal[i];
-						selectedArea = i;
-						minPrice =  PackageList[selectedArea].price;
-					}
-				}
-				
-			}
-			
-			//System.out.println("user:"+uid+" selected area:"+selectedArea +" ");
-			Visited[selectedArea] = 1;
-			if(PackageList[selectedArea] != null) {
-				//System.out.print("id:"+PackageList[selectedArea].id  +" ");
-				//if(first) {
-				System.out.println("---");
-					System.out.println("user:"+uid+" area: "+selectedArea+" recommend:"+PackageList[selectedArea].id);
-					//System.out.println("ll");
-				//	first = false;
-				//}
-				
-				
-				return PackageList[selectedArea].id ;
-			}
-			
-			//System.out.println();
-			checked++;
-			
-		}
-		//System.out.println("recommend fail");
-		return 0;
-		
-	}
+	
 }
 
 
